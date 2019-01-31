0e12254ba9c2855149e07f362808ed921f53910c
vivek2188 <tiwarivivekraj17@gmail.com>
2019-01-31 18:42:14 +0530

    missing values

--- Supervised/Basics/preprocessing.py
+++ Supervised/Basics/preprocessing.py
@@ -4,8 +4,14 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv("Data.csv")
 #print(dataset.head())
+print(dataset.isna().any())
 
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 # X,y are numpy ndarray(s)
 print("Input Shape: {}, Output Shape: {}".format(X.shape, y.shape))
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
