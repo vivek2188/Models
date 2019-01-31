2ba192521cf323e177af0874c90a87d201980cd8
vivek2188 <tiwarivivekraj17@gmail.com>
2019-01-31 18:56:09 +0530

    categorical features

--- Supervised/Basics/preprocessing.py
+++ Supervised/Basics/preprocessing.py
@@ -15,3 +15,11 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+lbl_x = LabelEncoder()
+X[:, 0] = lbl_x.fit_transform(X[:, 0])
+ohe = OneHotEncoder(categorical_features = [0])
+X = ohe.fit_transform(X).toarray()
+lbl_y = LabelEncoder()
+y = lbl_y.fit_transform(y)
