d8d5bc0cd9eb3d64d7699b374414006a3e539aa8
vivek2188 <tiwarivivekraj17@gmail.com>
2019-01-31 19:22:14 +0530

    scaling

--- Supervised/Basics/preprocessing.py
+++ Supervised/Basics/preprocessing.py
@@ -27,3 +27,9 @@ y = lbl_y.fit_transform(y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 10)
 print("Training Shape: {}, Test Shape: {}".format(X_train.shape, X_test.shape))
+
+from sklearn.preprocessing import StandardScaler
+scl_x = StandardScaler()
+X_train[:, 3:] = scl_x.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = scl_x.transform(X_test[:, 3:])
+print(X_train)
