98d44dac8348e1b8995e561e1cb0a00b257accda
vivek2188 <tiwarivivekraj17@gmail.com>
2019-01-31 18:34:10 +0530

    X and y's

--- Supervised/Basics/preprocessing.py
+++ Supervised/Basics/preprocessing.py
@@ -2,4 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+dataset = pd.read_csv("Data.csv")
+#print(dataset.head())
 
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+# X,y are numpy ndarray(s)
+print("Input Shape: {}, Output Shape: {}".format(X.shape, y.shape))
