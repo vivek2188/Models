107986c85d8539b6cff06794f1fd82540e6d2ee3
vivek2188 <tiwarivivekraj17@gmail.com>
2019-02-04 19:07:59 +0530

    minor

--- Supervised/Basics/LocallyWeightedRegression.py
+++ Supervised/Basics/LocallyWeightedRegression.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from numpy.linalg import pinv as pseudo_inverse
 
-def create_sine_data(min_x, max_x, n_datapoints = 300):
+def create_sine_data(min_x, max_x, n_datapoints = 500):
     X = np.linspace(min_x, max_x, n_datapoints)
     X = X.reshape(len(X), 1)
     y = np.sin(X)
@@ -19,19 +19,7 @@ class LWregressor:
         self.bandwidth_parameter = bandwidth_parameter
         
         self.J = list()
-
-    def compute_loss_and_gradient(self, iter_num, gradient = "batch"):
-        grad = np.zeros_like(self.theta)
         
-        if gradient == "batch":
-            error = np.matmul(self.X, self.theta) - self.y
-            cost = np.matmul(np.matmul(error.T, self.W), error).squeeze() / (2 * self.m)
-            
-            self.J.append([iter_num, float(cost)])
-        else:
-            pass
-        return grad
-    
     def normal_equation(self):
         _inverse = pseudo_inverse(np.matmul(np.matmul(self.X.T, self.W), X))
         _other = np.matmul(np.matmul(self.X.T, self.W), y)
@@ -47,15 +35,15 @@ class LWregressor:
         self._all_considered = np.ones(self.m)
         self.W = np.multiply(self._all_considered, self.mask)
 
-    def plot(self, x = None, plot = "unweighted"):
+    def predict(self, x = None, plot = "unweighted"):
         if plot == "unweighted":
             self._all_considered = np.ones(self.m)
             self.W = np.multiply(self._all_considered, self.mask)
 
             self.normal_equation() 
-            y_pred = np.matmul(self.X, self.theta)
+            self.y_pred = np.matmul(self.X, self.theta)
         
-            plt.plot(self.X[:, 1], y_pred, color = "green")
+            plt.plot(self.X[:, 1], self.y_pred, color = "green")
             plt.title("Linear (Unweighted) Regression using Normal Equation")
             
         elif plot == "weighted":
@@ -69,24 +57,22 @@ class LWregressor:
             self.W = np.multiply(self._all_considered, self.mask)
             
             self.normal_equation()
-            y_pred = np.matmul(X, self.theta)
+            self.y_pred = np.matmul(X, self.theta)
             
-            plt.plot(X[:, 1], y_pred, color = "blue")
+            plt.plot(X[:, 1], self.y_pred, color = "blue")
             plt.title("Locally Weighted Regression (bandwidth parameter = {})".format(self.bandwidth_parameter))
         else:
             print("ERROR: Enter valid \"plot\" parameter")
             return
-        
+
+    def visualize(self):
         # Visualize the data
         plt.xlabel("Input Feature")
         plt.ylabel("Predicted Value")
         plt.scatter(self.X[:, 1], self.y, s = 5, color = "red")
         plt.show()
-    
-    def predict(self, X):
-        pass
-    
 
+        
 if __name__ == "__main__":
     X, y = create_sine_data(-20, 20)
     X = np.hstack([np.ones((len(X), 1)), X])   # Intercept Column
@@ -95,4 +81,5 @@ if __name__ == "__main__":
     print("Ouput Shape: {}".format(y.shape))
     lr = LWregressor(bandwidth_parameter = 0.502)
     lr.fit(X, y)
-    lr.plot(x = X[0], plot = "weighted")
+    lr.predict(x = X[0], plot = "weighted")
+    lr.visualize()
